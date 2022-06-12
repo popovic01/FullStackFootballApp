b443420ac95e9671473a7e4207db3c77b27b3841
popovic01 <mimica2001@gmail.com>
2022-06-12 23:49:50 +0200

    Frontend done with changes

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
-export const LIGA_URL = 'http://localhost:8083/liga';
-export const NACIONALNOST_URL = 'http://localhost:8083/nacionalnost';
-export const IGRAC_URL = 'http://localhost:8083/igrac';
-export const IGRAC_URL_ZA_TIM = 'http://localhost:8083/igracTim';
-export const TIM_URL = 'http://localhost:8083/tim';
+export const LIGA_URL = 'https://rvait18.herokuapp.com/liga';
+export const NACIONALNOST_URL = 'https://rvait18.herokuapp.com/nacionalnost';
+export const IGRAC_URL = 'https://rvait18.herokuapp.com/igrac';
+export const IGRAC_URL_ZA_TIM = 'https://rvait18.herokuapp.com/igracTim';
+export const TIM_URL = 'https://rvait18.herokuapp.com/tim';
